5692249807b8838518a319c7a4652de4fd0c9969
Katariina Ingerma <katariina.ingerma@gmail.com>
2021-11-08 23:11:55 +0200

    =plot ei ava

---
+++ kkkkk.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plot
+import pandas as pd
+import numpy as np
+import time
+from tkinter import *
+from PIL import ImageTk, Image
+
+#proovi see lisada nupu juurde!!
+#proovi muuta nii et ei oleks ainult Friendside kohta
+def aeg():
+    data = pd.read_csv('KertuViewingActivity.csv')
+    data['Duration'] = pd.to_timedelta(data['Duration'])
+    sõbrad = data[data['Title'].str.contains('Friends', regex=False)]
+    sõbrad = sõbrad[(sõbrad['Duration'] > '0 days 00:01:00')]
+    kokku=sõbrad['Duration'].sum()
+    return kokku
+
+def send():
+    data = pd.read_csv('KertuViewingActivity.csv')
+    data['Duration'] = pd.to_timedelta(data['Duration'])
+    #sisend = input(str("Mis seriaal?"))
+    if sisend.get().lower() == "friends".lower():
+        sõbrad = data[data['Title'].str.contains('Friends', regex=False)]
+        sõbrad = sõbrad[(sõbrad['Duration'] > '0 days 00:01:00')]
+        kokku = print("Oled vaadanud seda: ", sõbrad['Duration'].sum())
+        return kokku
+    if sisend.get().lower() == "modern family".lower():
+        modernf = data[data['Title'].str.contains('Modern', regex=False)]
+        modernf = modernf[(modernf['Duration'] > '0 days 00:01:00')]
+        kokku = print("Oled vaadanud seda: ", modernf['Duration'].sum())
+        return kokku
+    if sisend.get().lower() == "The Big Bang Theory".lower():
+        TheBigBang = data[data['Title'].str.contains('The Big Bang Theory', regex=False)]
+        TheBigBang = TheBigBang[(TheBigBang['Duration'] > '0 days 00:01:00')]
+        kokku = print("Oled vaadanud seda: ", TheBigBang['Duration'].sum())
+        return kokku
+    
+    
+def film_või_sari(): 
+    data = pd.read_csv('KertuViewingActivity.csv')
+    pealkirjad = data.head(0)
+    sisu = data["Title"]
+    sarjad = 0
+    filmid = 0
+    for veerg in data["Title"]:
+        if "Season" in veerg:
+            sarjad += 1
+        else:
+            filmid += 1
+    y = np.array([sarjad, filmid])
+    tähistused = ["Sarjad", "Filmid"]
+    värvid = ["Black", "red"]
+    plot.pie(y, labels = tähistused, colors = värvid)
+    plot.show()
+
+
+    
+#if row (0) is not kertu delete row
+#def vaatamise_aeg():
+    
+raam = Tk()
+raam.configure(bg="white")
+raam.geometry("1000x800")
+pilt = PhotoImage(file="pilt.png")
+silt = Label(image=pilt)
+silt.pack()
+
+Button(raam, text="Filmide ja sarjade jaotus",
+       command= film_või_sari, bg="red", fg="black").pack(pady=20)
+#pady paneb ridadele vahed ja pack paneb asja keskele
+
+sisend = Entry(raam)
+sisend.pack()
+nupp = Button(raam, text="Saada", command=send, bg="red", fg="black").pack()
+
+raam.mainloop()
--- number1.py
+++ number1.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plot
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import pandas as pd
 import numpy as np
 from tkinter import *
@@ -7,29 +8,52 @@ from icrawler.builtin import GoogleImageCrawler
 import os
 import time
 
+
 raam = Tk()
-
-def uus_raam():
-    muuda_faili_nime()
-    time.sleep(1)
-    uusraam = Toplevel(raam) #toplevel uus aken
-    uusraam.title(str(sisend.get()))
-    uusraam.geometry("1000x600")
-    uus_pilt = PhotoImage(file="000001.png")  #mingi jama
-    uus = ImageTk.PhotoImage(uus_pilt)
-    uus_silt = Label(image=uus)
-    uus_silt.pack()
-
+sisend = Entry(raam, width=40, font="Graphique 15")
 def kasutaja_sisend():
     data = pd.read_csv('KertuViewingActivity.csv')
     uus_sisend = str(sisend.get())
-    google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\katar\OneDrive\Desktop\Netflix'})
-    google_Crawler.crawl(keyword = uus_sisend, max_num = 1)
-    
-    for veerg in data["Title"]:
-        if uus_sisend in veerg:
-            print("jes")
+    #google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\katar\OneDrive\Desktop\Netflix'})
+    #google_Crawler.crawl(keyword = uus_sisend, max_num = 1)
     return uus_raam()
+
+
+def uus_raam():
+    #muuda_faili_nime()
+    uusraam = Toplevel(raam) #toplevel uus aken
+    uusraam.title(str(sisend.get()))
+    uusraam.geometry("1000x600")
+    #uus_pilt = PhotoImage(file="000001.png")  #mingi jama
+    #uus = ImageTk.PhotoImage(uus_pilt)
+    #uus_silt = Label(image=uus)
+    #uus_silt.pack()
+    jama = Label(uusraam, text = send())
+    
+    jama.pack()
+    film_või_sari()
+def send():
+    data = pd.read_csv('KertuViewingActivity.csv')
+    data['Duration'] = pd.to_timedelta(data['Duration'])
+    andmed = data["Title"]
+    kasutaja_sisend = str(sisend.get())
+    for veerg in andmed:
+        if kasutaja_sisend in veerg:
+            aeg = data[data['Title'].str.contains(kasutaja_sisend, regex=False)]
+            aeg = aeg[(aeg['Duration'] > '0 days 00:01:00')]
+            kokku = ("Oled vaadanud seda: ", aeg['Duration'].sum())
+            return str(kokku)
+
+
+#proovi see lisada nupu juurde!!
+#proovi muuta nii et ei oleks ainult Friendside kohta
+def aeg():
+    data = pd.read_csv('KertuViewingActivity.csv')
+    data['Duration'] = pd.to_timedelta(data['Duration'])
+    sõbrad = data[data['Title'].str.contains('Friends', regex=False)]
+    sõbrad = sõbrad[(sõbrad['Duration'] > '0 days 00:01:00')]
+    kokku=sõbrad['Duration'].sum()
+    return kokku
 
 
 def muuda_faili_nime():
@@ -58,6 +82,19 @@ def film_või_sari():
     värvid = ["Black", "red"]
     plot.pie(y, labels = tähistused, colors = värvid)
     plot.show()
+def üldine_aken():
+    plot1=film_või_sari()
+    canvas = FigureCanvasTkAgg(plot1, master = raam)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    toolbar = NavigationToolbar2TkAgg(canvas, self)
+    toolbar.update()
+    canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    #uusraam = Toplevel(raam)
+    #uusraam.geometry("1000x600")
+    #uusraam.title(text="Üldised andmed")
+    
    
 raam.call('wm', 'iconphoto', raam._w, PhotoImage(file='n.png'))
 raam.title("Netflix andmeanalüüs")
@@ -73,6 +110,6 @@ sisendi_silt = Label(raam, text="Kirjuta siia sarja või filmi nimi:", bg ="whit
 sisendi_silt.pack(pady=20)
 sisend = Entry(raam, width=40, font="Graphique 15")
 sisend.pack()
-nupp = Button(raam, text="Näita", command=kasutaja_sisend, bg="white", fg="black", font="Graphique 15").pack()
-
+nupp = Button(raam, text="Näita", command=kasutaja_sisend, bg="black", fg="red2", font="Graphique 15").pack()
+Üldised_andmed= Button(raam, text="Kõik andmed", command=üldine_aken, bg="black",  fg="red2", font="Graphique 15").pack(pady=20)
 raam.mainloop()
