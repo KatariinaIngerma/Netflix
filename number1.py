1f2847685252185d2008ef476d83cc8a7373376d
Katariina Ingerma <katariina.ingerma@gmail.com>
2021-11-10 18:17:31 +0200

    =viimane

--- number1.py
+++ number1.py
@@ -8,30 +8,34 @@ from icrawler.builtin import GoogleImageCrawler
 import os
 import time
 
-
 raam = Tk()
 sisend = Entry(raam, width=40, font="Graphique 15")
 def kasutaja_sisend():
+    kustuta_pilt()
     data = pd.read_csv('KertuViewingActivity.csv')
     uus_sisend = str(sisend.get())
-    #google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\katar\OneDrive\Desktop\Netflix'})
-    #google_Crawler.crawl(keyword = uus_sisend, max_num = 1)
+    google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\katar\OneDrive\Desktop\Netflix'})
+    #google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\Külmkapp\OneDrive\Desktop\Netflix'})
+    google_Crawler.crawl(keyword = uus_sisend, max_num = 1)
     return uus_raam()
 
-
 def uus_raam():
-    #muuda_faili_nime()
+    uus_nimi()
     uusraam = Toplevel(raam) #toplevel uus aken
     uusraam.title(str(sisend.get()))
     uusraam.geometry("1000x600")
-    #uus_pilt = PhotoImage(file="000001.png")  #mingi jama
-    #uus = ImageTk.PhotoImage(uus_pilt)
-    #uus_silt = Label(image=uus)
-    #uus_silt.pack()
-    jama = Label(uusraam, text = send())
+    #uus_pilt = PhotoImage(file = "000001.png")  #mingi jama pilt ei tule kuidagi :dddd
+#     img = Image.open('000001.png')
+#     photo = ImageTk.PhotoImage(img)
+#     silt = Label(image=photo)
+#     silt.pack()
     
+#     uus = ImageTk.PhotoImage(uus_pilt)
+#     uus_silt = Label(image=uus)
+#     uus_silt.pack()
+    jama = Label(uusraam, text = send())
     jama.pack()
-    film_või_sari()
+    
 def send():
     data = pd.read_csv('KertuViewingActivity.csv')
     data['Duration'] = pd.to_timedelta(data['Duration'])
@@ -42,29 +46,33 @@ def send():
             aeg = data[data['Title'].str.contains(kasutaja_sisend, regex=False)]
             aeg = aeg[(aeg['Duration'] > '0 days 00:01:00')]
             kokku = ("Oled vaadanud seda: ", aeg['Duration'].sum())
-            return str(kokku)
+            return kokku
+        if kasutaja_sisend not in veerg:
+            kokku = "Sa pole seda vaadanud :("
+            return kokku
+        
 
-
-#proovi see lisada nupu juurde!!
-#proovi muuta nii et ei oleks ainult Friendside kohta
 def aeg():
     data = pd.read_csv('KertuViewingActivity.csv')
     data['Duration'] = pd.to_timedelta(data['Duration'])
-    sõbrad = data[data['Title'].str.contains('Friends', regex=False)]
-    sõbrad = sõbrad[(sõbrad['Duration'] > '0 days 00:01:00')]
-    kokku=sõbrad['Duration'].sum()
+    aeg = data[data['Title'].str.contains(sisend, regex=False)]
+    aeg = aeg[(aeg['Duration'] > '0 days 00:01:00')]
+    kokku=aeg['Duration'].sum()
     return kokku
 
-
-def muuda_faili_nime():
-    uus_nimi = r'C:\Users\katar\OneDrive\Desktop\Netflix\000001.png'
-    vana_nimi = r'C:\Users\katar\OneDrive\Desktop\Netflix\000001.jpg'
-    if os.path.isfile(uus_nimi):
-        print("ok")
-    else:
-        if os.path.isfile(vana_nimi):
-            im1 = Image.open(r'C:\Users\katar\OneDrive\Desktop\Netflix\000001.jpg')
-            im1.save(r'C:\Users\katar\OneDrive\Desktop\Netflix\000001.png')
+def uus_nimi():
+    try: 
+        vana_nimi = "000001.jpg"
+        base = os.path.splitext(vana_nimi)[0]
+        os.rename(vana_nimi, base + ".png")
+    except:
+        vana_nimi = "000001.jpeg"
+        base = os.path.splitext(vana_nimi)[0]
+        os.rename(vana_nimi, base + ".png")
+        
+def kustuta_pilt():
+    if os.path.exists("000001.png"):
+        os.remove("000001.png")
 
 def film_või_sari(): 
     data = pd.read_csv('KertuViewingActivity.csv')
@@ -82,6 +90,14 @@ def film_või_sari():
     värvid = ["Black", "red"]
     plot.pie(y, labels = tähistused, colors = värvid)
     plot.show()
+    
+def kogu_aeg():
+    data = pd.read_csv('KertuViewingActivity.csv')
+    kokku=pd.to_timedelta(data['Duration']).sum()
+    print(kokku)
+    return kokku
+kogu_aeg()    
+    
 def üldine_aken():
     plot1=film_või_sari()
     canvas = FigureCanvasTkAgg(plot1, master = raam)
@@ -94,7 +110,6 @@ def üldine_aken():
     #uusraam = Toplevel(raam)
     #uusraam.geometry("1000x600")
     #uusraam.title(text="Üldised andmed")
-    
    
 raam.call('wm', 'iconphoto', raam._w, PhotoImage(file='n.png'))
 raam.title("Netflix andmeanalüüs")
@@ -104,12 +119,12 @@ pilt = PhotoImage(file="pilt.png")
 silt = Label(image=pilt)
 silt.pack()
 
-#Button(raam, text="Filmide ja sarjade jaotus",command= film_või_sari,
-      # bg="red2", fg="white", font="Graphique 15").pack(pady=20) #pady paneb ridadele vahed ja pack paneb asja keskele
-sisendi_silt = Label(raam, text="Kirjuta siia sarja või filmi nimi:", bg ="white", fg="black", font="Graphique 15")
+# Button(raam, text="Filmide ja sarjade jaotus",command= film_või_sari,
+#        bg="red2", fg="white", font="Graphique 15").pack(pady=20) #pady paneb ridadele vahed ja pack paneb asja keskele
+sisendi_silt = Label(raam, text="Kirjuta siia sarja või filmi nimi:", bg ="white", fg="red", font="Graphique 15")
 sisendi_silt.pack(pady=20)
 sisend = Entry(raam, width=40, font="Graphique 15")
 sisend.pack()
-nupp = Button(raam, text="Näita", command=kasutaja_sisend, bg="black", fg="red2", font="Graphique 15").pack()
-Üldised_andmed= Button(raam, text="Kõik andmed", command=üldine_aken, bg="black",  fg="red2", font="Graphique 15").pack(pady=20)
+nupp = Button(raam, text="Näita", command=kasutaja_sisend, bg="white", fg="red2", font="Graphique 15").pack()
+Üldised_andmed= Button(raam, text="Kõik andmed", command=üldine_aken, bg="white",  fg="red2", font="Graphique 15").pack(pady=20)
 raam.mainloop()
