cb68eeb52edc18f54f9bc6592146d2efbe18731e
Kata <92819204+KatariinaIngerma@users.noreply.github.com>
2021-12-19 13:11:10 +0200

    Delete piltgooglest4.py

--- piltgooglest4.py
+++
@@ -1,9 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
-absolute_image_paths = response.download({
-                "keywords": "kass",
-                 "format": "png",
-                 "limit":1,
-                 "size": "medium",
-                 "aspect_ratio":"panoramic"})
