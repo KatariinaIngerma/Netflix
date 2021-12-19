a8f9409d4e6f4572b87f6cf2b0fff1cb08263ac8
Kata <92819204+KatariinaIngerma@users.noreply.github.com>
2021-12-19 13:16:15 +0200

    Delete liigne directory

--- liigne/graafikute katse.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig, (ax1, ax2) = plt.subplots(1,2)
-fig.suptitle("sarjad ja filmid")
-x = np.linspace(0, 2 * np.pi, 400)
-y = np.sin(x ** 2)
-ax1.plot(x,y)
-ax2.plot(x,-y)
